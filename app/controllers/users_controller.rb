8ad5717048b9d166d887ea052b6f14be99b515cd
lkvsky <kyle@lucovsky.com>
2013-02-08 09:14:31 -0800

    edit and update actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,19 @@ class UsersController < ApplicationController
     redirect_to new_session_path
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user][:id])
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.save!
+
+    redirect_to edit_quiz_path
+  end
+
   def destroy
     @user = User.find(params[:id])
     flash[:notice] = "user destroyed"
