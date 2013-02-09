d39f96bdfa198b971106390b586b0fc8391e20a5
lkvsky <kyle@lucovsky.com>
2013-02-08 18:09:06 -0800

    user can log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,26 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:user][:email])
     session_token = SecureRandom.urlsafe_base64
+    @user.session_token = session_token
 
     if @user.nil?
       flash[:notice] = "Looks like you don't exist yet."
     end
     
-    begin
-      @user.authenticate(params[:user][:password])
-      @user.session_token = session_token
-      @user.save!
+    if @user.save && @user.authenticate(params[:user][:password])
       session[:session_token] = session_token
-      redirect_to edit_quiz_path
-    rescue
+      redirect_to quizzes_path
+    else
       flash[:notice] = "Looks like you don't exist yet."
       redirect_to root_path
     end
+  end
+
+  def destroy
+    user = User.find_by_session_token(session[:session_token])
+    user.log_out
+    session[:session_token] = nil
+
+    redirect_to root_path
   end
 end
